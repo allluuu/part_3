c3bef1d3fa8501b272947af115afdd2c47a62929
allluuu <alosah@utu.fi>
2019-03-05 14:27:57 +0200

    trying to handle delete

---
+++ build/precache-manifest.f02226575c62c51c01213ee6d188a5ef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7c7c6d4c157f0d4b074",
+    "url": "/static/js/main.a7c7c6d4.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "52fbb79f2c8ddbb7f9bc",
+    "url": "/static/js/2.52fbb79f.chunk.js"
+  },
+  {
+    "revision": "c61b24758d68a00032806d8a607d8f0a",
+    "url": "/index.html"
+  }
+];
