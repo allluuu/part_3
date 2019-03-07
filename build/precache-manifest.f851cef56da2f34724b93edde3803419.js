1083bfc891f32a69561adbd7f55ed6a059622bcd
allluuu <alosah@utu.fi>
2019-03-07 13:59:01 +0200

    make it better

---
+++ build/precache-manifest.f851cef56da2f34724b93edde3803419.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e34d7c021e7fb5e92f06",
+    "url": "/static/js/main.e34d7c02.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "52fbb79f2c8ddbb7f9bc",
+    "url": "/static/js/2.52fbb79f.chunk.js"
+  },
+  {
+    "revision": "68434cf734505732740f09229a8d2a3c",
+    "url": "/index.html"
+  }
+];
